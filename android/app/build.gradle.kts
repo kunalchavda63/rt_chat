5f9fcf8bfdb2a3ba484484c677975df0a127c923
kunalchavda63 <130273991+kunalchavda63@users.noreply.github.com>
2025-06-19 17:57:36 +0530

    Replace Android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,5 @@
 plugins {
     id("com.android.application")
-    // START: FlutterFire Configuration
-    id("com.google.gms.google-services")
-    // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
@@ -18,9 +15,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
 
-    ndkVersion = "27.0.12077973"
-
-
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
@@ -30,7 +24,7 @@ android {
         applicationId = "com.example.rt_chat"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
